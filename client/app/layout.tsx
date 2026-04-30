12401be6cb397310ab83a853574a9eb1dc2eafad
Martin-Kaze <manvispa1@gmail.com>
2026-04-30 16:29:04 +0300

    added posthog

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import StoreProvider from "@/state/stateProvider";
+import PostHogPageView from "./PostHobPageView"; 
 import { Public_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 
@@ -15,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 <meta name="msvalidate.01" content="3788715B74BE2070194BA1B799D2AEA5" />
       <body className="min-h-screen bg-amber-50 font-sans text-foreground ">
         <StoreProvider>
-          
+          <PostHogPageView/>
             {children}
        
         </StoreProvider>
--- client/next.config.ts
+++ client/next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
    allowedDevOrigins: ["192.168.0.100"],
+   
    async rewrites() {
     return [
       {
         source: '/api/:path*',
-        // Use local port in dev, production URL in prod
         destination: process.env.NODE_ENV === 'development' 
           ? 'http://127.0.0.1:3500/api/:path*'
           : 'https://your-api-domain.com/api/:path*', 
