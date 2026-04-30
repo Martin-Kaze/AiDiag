4a53d0c4d73a70214a9ed679785fc13450f65db2
Martin-Kaze <manvispa1@gmail.com>
2026-04-30 15:38:33 +0300

    added header

---
+++ client/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="msvalidate.01"
+        content="3788715B74BE2070194BA1B799D2AEA5"
+      />
+    </>
+  );
+}
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
 
     <html lang="en" className={cn("antialiased", publicSans.variable)}>
-
+<meta name="msvalidate.01" content="3788715B74BE2070194BA1B799D2AEA5" />
       <body className="min-h-screen bg-amber-50 font-sans text-foreground ">
         <StoreProvider>
           
