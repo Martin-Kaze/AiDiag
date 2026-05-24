4d65bfd049b652a51593f4f9bb079589c4902df9
Martin-Kaze <manvispa1@gmail.com>
2026-05-24 11:59:35 +0300

    removed old config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   allowedDevOrigins: ["192.168.0.100"],
-   
-   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: process.env.NODE_ENV === 'development' 
-          ? 'http://127.0.0.1:3500/api/:path*'
-          : 'https://your-api-domain.com/api/:path*', 
-      },
-    ];
-  },
+  allowedDevOrigins: ["192.168.0.100"],
 };
 
 export default nextConfig;
