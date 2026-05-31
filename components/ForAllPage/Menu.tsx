05dac3512e68ba73d830a13e7aea20ea16fe1168
Martin-Kaze <manvispa1@gmail.com>
2026-05-31 15:09:10 +0300

    made menu bar optinal in the header

--- components/ForAllPage/Menu.tsx
+++ components/ForAllPage/Menu.tsx
@@ -4,11 +4,11 @@ import { Menubar, MenubarMenu } from "@/components/ui/menubar"
 import { SidebarTrigger } from "@/components/ui/sidebar"; // Only one import allowed
 import Link from "next/link"
 
-export function Menu() {
+export function Menu( props : { nosidebar? : boolean} ) {
   return (
-    <Menubar className="relative p-2 bg-background w-full rounded-none border-b">
+    <Menubar className="relative p-2 py-5 bg-background w-full rounded-none border-b">
       <MenubarMenu>
-        <SidebarTrigger />
+        {!props.nosidebar ? <SidebarTrigger /> : null }
       </MenubarMenu>
       <Link href="/" className="absolute left-1/2 -translate-x-1/2 text-xl font-bold uppercase">
         Wellness
