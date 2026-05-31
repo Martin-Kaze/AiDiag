b5d8d0374f4d18b19f9c9bdde0978f30fbdf9843
Martin-Kaze <manvispa1@gmail.com>
2026-05-31 15:08:42 +0300

    remove unnecessary files

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,17 +1,10 @@
-"use client";
 
-import { useChat  } from '@ai-sdk/react';
-import { Menu } from "@/components/ForAllPage/Menu";
-import { useState, SubmitEvent } from "react";
-import type { UIMessage } from "ai";
 
 export default function Page() {
-
+  
   return (
-    <div className="flex flex-col min-h-screen w-full">
-
-      <header className="w-full"><Menu /></header>
-
-    </div>
-  );
+  <>
+  
+  </>
+  )
 }
