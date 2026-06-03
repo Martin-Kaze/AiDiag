9efebb12c700a60b36d4117ce1fe393f377116db
Martin-Kaze <manvispa1@gmail.com>
2026-06-03 20:10:12 +0300

    other fix

--- app/login/components/login-form.tsx
+++ app/login/components/login-form.tsx
@@ -52,7 +52,7 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={(e) => e.preventDefault()}>
             <FieldGroup>
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Welcome</h1>
