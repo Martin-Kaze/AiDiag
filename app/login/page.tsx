a53949a3f8f20fe4ec9f500245e76d04918eacc7
Martin-Kaze <manvispa1@gmail.com>
2026-06-03 19:37:57 +0300

    fixed build problems

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 
 import { Menu } from "@/components/ForAllPage/Menu";
 import { Footer } from "@/components/ForAllPage/Footer";
@@ -6,21 +7,14 @@ import { LoginForm } from "@/app/login/components/login-form";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen w-full">
-
       <header className="w-full"><Menu /></header>
-      
-
-      <main className=" relative flex flex-1 flex-col  justify-center gap-4  mx-auto">
-
-     
- <p className="text-3xl font-bold text-center mb-10"> Your subscriptions, analyzed for your wellbeing. </p>
-
-<LoginForm className="mx-1"/> 
-     
+      <main className="relative flex flex-1 flex-col justify-center gap-4 mx-auto">
+        <p className="text-3xl font-bold text-center mb-10">
+          Your subscriptions, analyzed for your wellbeing.
+        </p>
+        <LoginForm className="mx-1" />
       </main>
-
-     <Footer/>
-
+      <Footer />
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   serverExternalPackages: ["better-auth", "@better-auth/kysely-adapter", "kysely"],
   allowedDevOrigins: ["192.168.0.100"],
 };
 
