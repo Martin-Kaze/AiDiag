4b51425665bcfc492edaff31626b8286ac81d149
Martin-Kaze <manvispa1@gmail.com>
2026-06-04 17:20:04 +0300

    minor login page change, better for small screens

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,28 @@
-export const dynamic = "force-dynamic";
 
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { LoginForm } from "./components/login-form";
 import { Menu } from "@/components/ForAllPage/Menu";
 import { Footer } from "@/components/ForAllPage/Footer";
-import { LoginForm } from "@/app/login/components/login-form";
+export default async function LoginPage() {
+  const session = await auth.api.getSession({ 
+    headers: await headers()  
+  });
 
-export default function Home() {
+  if (session) redirect("/dashboard"); 
+
   return (
-    <div className="flex flex-col min-h-screen w-full">
-      <header className="w-full"><Menu /></header>
-      <main className="relative flex flex-1 flex-col justify-center gap-4 mx-auto">
-        <p className="text-3xl font-bold text-center mb-10">
-          Your subscriptions, analyzed for your wellbeing.
-        </p>
-        <LoginForm className="mx-1" />
-      </main>
-      <Footer />
-    </div>
+   <div className="flex flex-col min-h-screen w-full">
+
+  <header className="w-full"><Menu /></header>
+  
+  <div className="flex flex-1 items-center justify-center">
+    <LoginForm className="size-70 md:size-150" />
+  </div>
+   
+  <Footer/>
+</div>
+   
   );
 }
