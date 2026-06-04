3b6dd8b15c3e3e985062f6e132dcc48d56f162e9
Martin-Kaze <manvispa1@gmail.com>
2026-06-04 17:47:06 +0300

    changes for google brand verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,23 @@
 import "./globals.css";
 import StoreProvider from "@/state/stateProvider";
-import PostHogPageView from "./PostHobPageView"; 
 import { Public_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Suspense } from "react";
 import { AppSidebar } from "@/components/ForAllPage/AppSidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { Toaster } from '@/components/ui/sonner'
-import Script from "next/script"; 
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { Toaster } from "@/components/ui/sonner";
+import Script from "next/script";
+
+export const metadata = {
+  title: "Wellness.chat",
+  description: "Analyse your behaviour, social media, and daily patterns to help you become calmer, healthier and more at peace.",
+  verification: {
+    google: "SSJZMxzrCMzs0QFxmKBIt4koAYUFh9ZNrH50r2FF_28",
+    other: {
+      "msvalidate.01": "3788715B74BE2070194BA1B799D2AEA5",
+    },
+  },
+};
 
 const publicSans = Public_Sans({
   subsets: ["latin"],
@@ -18,9 +28,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={cn("antialiased", publicSans.variable)}>
       <head>
-        <meta name="msvalidate.01" content="3788715B74BE2070194BA1B799D2AEA5" />
-        <meta name="google-site-verification" content="SSJZMxzrCMzs0QFxmKBIt4koAYUFh9ZNrH50r2FF_28" />
-        {/* Google Analytics - G-QQTSCDPZXN */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-QQTSCDPZXN"
           strategy="afterInteractive"
@@ -34,20 +41,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           `}
         </Script>
       </head>
-      <body className="min-h-screen  bg-amber-50 font-sans text-foreground ">
+      <body className="min-h-screen bg-amber-50 font-sans text-foreground">
         <StoreProvider>
-          <Suspense fallback={null}>
- 
-          </Suspense>
+          <Suspense fallback={null} />
           <SidebarProvider defaultOpen={false}>
             <div className="fixed inset-y-0 left-0 z-50">
-    <AppSidebar />
-  </div>
-            
-          {children}
+              <AppSidebar />
+            </div>
+            {children}
           </SidebarProvider>
         </StoreProvider>
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
   );
