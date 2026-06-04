8b754d3769f84aff7aa627c8f2e10a2ca1221612
Martin-Kaze <manvispa1@gmail.com>
2026-06-04 17:18:09 +0300

    minor hero changes, made more simple

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
 import { Hero } from "@/components/Hero";
 import { Menu } from "@/components/ForAllPage/Menu";
+import { Footer } from "@/components/ForAllPage/Footer";
 
 export default function Page() {
-  
+
   return (
-  <>
-   <div className="flex flex-col min-h-screen w-full">
-  <Menu nosidebar={true}/>
-        <main >
-        <Hero className=""/>;
 
-        </main>
-  
+    <div className="flex flex-col min-h-screen w-full">
+      <Menu nosidebar={true} />
+      <main >
+        <Hero className="" />
+      </main>
+      <Footer />
+    </div>
 
- 
-
-  </div>
-  </>
   )
 }
