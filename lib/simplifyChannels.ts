668c57a343fab19d65a77654e715ac6ea275e42f
Martin-Kaze <manvispa1@gmail.com>
2026-07-04 19:32:47 +0300

    delete : removing old yt fething and login apges, and redirect

--- lib/simplifyChannels.ts
+++
@@ -1,7 +0,0 @@
-
-export function simplifyChannels(data: any[]) {
-  return data.map((item) => ({
-    name: item.snippet?.title ?? "",
-    channelId: item.snippet?.resourceId?.channelId ?? "",
-  }));
-}
